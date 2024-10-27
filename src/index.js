4719326e9321f05e4c547db4038ca1154198021b
Omar Monib <omar.monib.03@gmail.com>
2024-10-27 11:46:00 +0300

    edit basename

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'; // استيراد createRoot
+import { BrowserRouter } from 'react-router-dom';
 import App from './pages/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // استخدام createRoot
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <BrowserRouter basename="/omarmonib/radio-pro.git">
+  <App />
+</BrowserRouter>
 );
